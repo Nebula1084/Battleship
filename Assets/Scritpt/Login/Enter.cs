fe4c60220e58ace6dc17ed4e1bb0c007fa2a7a14
Sea <hjw_redeption@outlook.com>
2016-05-25 21:45:42 -1200

    login scene

---
+++ Assets/Scritpt/Login/Enter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Enter::Start()");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void Awake()
+	{
+		GameObject button = GameObject.Find("UI Root/Enter");
+		UIEventListener.Get(button).onClick = ButtonClick;
+		Debug.Log ("Enter::Awake()");
+	}
+	
+	void ButtonClick(GameObject button)
+	{
+		Debug.Log("NGUI button name :"+button.name);
+	}
+}
